74553f2f9cf1c7e8bcee1e5997b9f1fb49acd87d
Joseph Benkanoun <benkanoun.greene@gmail.com>
2024-02-09 15:44:08 +0000

    week 02

---
+++ week02/addOne.py
@@ -0,0 +1,7 @@
+# addOne.py
+# author: Joseph Benkanoun
+# reads a number and prints number + 1
+
+number = int(input("please enter a number: "))
+newNumber = number + 1
+print(f"{number} plus one is {newNumber}")
